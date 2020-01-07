f8bb4c5df4b285f222e9e5aa6ceaedc944d7fdab
unknown <daviteixeira.btm@gmail.com>
2020-01-07 00:42:23 -0200

    Fundamentos.

---
+++ JavaScript Fundamentos/aula01.js
@@ -0,0 +1,23 @@
+// Variaveis e Constantes
+
+var a = 3
+let b = 4
+
+// Criar variaveis usando "let" é uma forma mais moderna.
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+/* As constantes "const", sao palavras reservadas que não deixam atribuir um novo valor. Sempre que for trabalhar
+com algum tipo de variavel que ela não for mudar o seu valor, use as const para ficar mais facil de manter o programa */
+
+const c = 5
+// c = 50
+console.log(c)
---
+++ JavaScript Fundamentos/aula02.js
@@ -0,0 +1,14 @@
+// Tipagem Fraca
+
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer) // Typeof mostra o tipo da variavel
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// Evitar nome genéricos e siglas
+let valor = ''
+let numero = 1
+let pqp = false
