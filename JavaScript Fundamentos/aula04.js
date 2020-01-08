3d43c529ee9fddb789dfb0ba252a67d2812ae33b
unknown <daviteixeira.btm@gmail.com>
2020-01-08 21:55:39 -0200

    Fundamentos.

---
+++ JavaScript Fundamentos/aula04.js
@@ -0,0 +1,8 @@
+console.log(7 / 0) // JavaScript tem o tipo infinito, o resultado da operação
+console.log("10" / 2) //Entre aspas simples ou duplas ele vai converter a String pra number e vai dividir
+console.log('3' + 2) // string ganha!
+console.log('3' - 2)
+console.log("Show!" * 2)
+console.log(0.1 + 0.7) // Isso não resulta em 0.8
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
