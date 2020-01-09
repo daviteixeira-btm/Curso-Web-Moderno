76919ed5a7caaa15c1984a664a0fda649eb65c3b
unknown <daviteixeira.btm@gmail.com>
2020-01-08 22:50:55 -0200

    Fundamentos.

---
+++ JavaScript Fundamentos/aula05.js
@@ -0,0 +1,10 @@
+// Metodo MATH
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+/* Math é um objeto e tem uma constante chamada PI, alem de ter uma função para
+calcular potencias (pow) */
+
+console.log(area)
+console.log(typeof Math) // Objeto.
