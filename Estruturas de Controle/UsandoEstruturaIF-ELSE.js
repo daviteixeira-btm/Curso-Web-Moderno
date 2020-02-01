df8d12db2db8d5f9ca6fbf1006dbecd96f650340
unknown <daviteixeira.btm@gmail.com>
2020-01-31 23:49:00 -0300

    Estruturas de Controle.

---
+++ Estruturas de Controle/UsandoEstruturaIF-ELSE.js
@@ -0,0 +1,15 @@
+// Usando Estrutura IF/ELSE
+
+const imprimirResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!') // cuidado!!!
+
+/* JavaScript é uma linguagem fracamente tipada */
