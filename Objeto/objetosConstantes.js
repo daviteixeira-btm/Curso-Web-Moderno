00f00631adf47e4deedc859958078ea9ffe2f095
unknown <daviteixeira.btm@gmail.com>
2020-02-10 01:10:39 -0300

    Objeto.

--- Objeto/objetosConstantes.js
+++ Objeto/objetosConstantes.js
@@ -6,15 +6,15 @@ console.log(pessoa)
 // pessoa -> 456 -> {...}
 // pessoa = { nome: 'Ana' }
 
-Object.freeze(pessoa)
+Object.freeze(pessoa) //uma vez que le ta congelado, vc não consegue mexer mais no objeto.
 
-pessoa.nome = 'Maria'
-pessoa.end = 'Rua ABC'
-delete pessoa.nome
+pessoa.nome = 'Maria' // ele vai ignorar, continuando sendo pedro.
+pessoa.end = 'Rua ABC' // tambem não da para dicionar endereço
+delete pessoa.nome // tambem não da para excluir o objeto.
 
 console.log(pessoa.nome)
 console.log(pessoa)
 
-const pessoaConstante = Object.freeze({ nome: 'Joao' })
-pessoaConstante.nome = 'Maria'
+const pessoaConstante = Object.freeze({ nome: 'Joao' }) // criando desde o começo um objeto constante.
+pessoaConstante.nome = 'Maria' // não funciona.
 console.log(pessoaConstante)
