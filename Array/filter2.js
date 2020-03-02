3933b514542022680a068bea3691df87bd105bef
Davi <daviteixeira.btm@gmail.com>
2020-03-01 23:18:49 -0300

    Array.

--- Array/filter2.js
+++ Array/filter2.js
@@ -1,6 +1,12 @@
-/**
- * Server para filtrar uma array, gerando arrays menores.
- */
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        if(callback(this[i], i, this)) {
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
 
 const produtos = [
     { nome: 'Notebook', preco: 2499, fragil: true },
@@ -9,11 +15,7 @@ const produtos = [
     { nome: 'Copo de Plástico', preco: 18.99, fragil: false }
 ]
 
-console.log(produtos.filter(function(p) { // p representa cada um dos produtos da array
-    return false // significa que para cada elemento recebido ele nao vai estar presente no array final.
-}))
-
 const caro = produto => produto.preco >= 500
 const fragil = produto => produto.fragil
 
-console.log(produtos.filter(caro).filter(fragil))
+console.log(produtos.filter2(caro).filter2(fragil))
